e0ac909b303440e9f30b74bd338f91def832a028
ldhmachin <ldhmachin@gmail.com>
2020-07-04 02:52:31 +0100

    Added code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dir-css
 Stylesheet for displaying a HTML table list of files
+## Using the converter Ruby script
+As this stylesheet is for use with file directories, the converter script requires the last cells on each row (excluding header) of the imported CSV file to be a file path. Use the mockaroo.csv example as a reference.
+
+Run the `convert.rb` script through the command line:
+```
+ruby ./convert.rb <input csv path> <export html path> <optional title>
+```
+Example:
+```
+ruby ./convert.rb ./mockaroo.csv ./index.html "Repository"
+ruby ./convert.rb ./example.csv ./index.html
+```
---
+++ convert.rb
@@ -0,0 +1,34 @@
+# Return if no arguments given
+return if ARGV.length <= 1
+# Get arguments
+input_path = ARGV[0]
+export_path = ARGV[1]
+title = ARGV.length > 2 ? ARGV[2] : ""
+# Return if invalid file path
+return unless File.exist?(input_path)
+# Set values
+lines = File.readlines(input_path)
+columns = lines.last.split(",").length
+# Create HTML title table
+html_title = title == "" ? "" : "<table class=\"title\"><tr><td>#{title}</td></tr></table>"
+# Create HTML content table
+html_table = "<table class=\"dir\">"
+lines.each_with_index do |line, row|
+    tag = (row == 0) ? "thead" : "tbody"
+    cells = line.chomp.split(",")
+    html_row = cells.map.with_index do |cell, column| 
+        if ((column == 0) && (row > 0))
+            "<td><a href=\"#{cells.last}\">#{cell}</a></td>"
+        elsif (column != columns - 1)
+            "<td>#{cell}</td>"
+        end
+    end
+    html_table += "<#{tag}><tr>#{html_row.join}</tr></#{tag}>"
+end
+html_table += "</table>"
+# Construct HTML
+html_head = "<head><title>Dir CSS</title><link rel=\"stylesheet\" type=\"text/css\" href=\"dir.css\"></head>"
+html_body = "<body>#{html_title}#{html_table}</body>"
+html = "<!DOCTYPE html><html>#{html_head}#{html_body}</html>"
+# Write to file
+File.write(export_path, html)
